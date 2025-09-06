bdec0dce64bfee4e9075c15885e58bc7067fda68
Sadra Karami <im100ra@gmail.com>
2025-09-06 11:58:51 +0330

    fix(auth): fix left side height in auth layout

--- src/app/(auth)/left-side.tsx
+++ src/app/(auth)/left-side.tsx
@@ -1,6 +1,6 @@
 export function LeftSide() {
   return (
-    <div className="relative  hidden w-1/2 items-center justify-center bg-gradient-to-br from-cyan-500 via-sky-600 to-indigo-700 lg:flex overflow-hidden">
+    <div className="relative h-screen  hidden w-1/2 items-center justify-center bg-gradient-to-br from-cyan-500 via-sky-600 to-indigo-700 lg:flex overflow-hidden">
       <div className="absolute -top-24 -left-24 h-96 w-96 rounded-full bg-white/10 blur-3xl"></div>
       <div className="absolute bottom-0 right-0 h-80 w-80 rounded-full bg-indigo-400/20 blur-2xl"></div>
 
